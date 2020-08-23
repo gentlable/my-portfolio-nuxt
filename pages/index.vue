43d25d889a5d883ae3f1fc87865772d1dd679a5b
gentlable <35590706+gentlable@users.noreply.github.com>
2020-08-23 23:14:59 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,10 @@
     <about-section />
     <test-section />
     -->
+    <div class="container">
+      タイトル:{{ posts.title }}
+      <p v-html="posts.body" />
+    </div>
   </div>
 </template>
 
@@ -14,6 +18,7 @@ import TitleSection from '~/components/templates/TitleSection.vue'
 import WorksSection from '~/components/templates/WorksSection.vue'
 import AboutSection from '~/components/templates/AboutSection.vue'
 import TestSection from '~/components/templates/TestSection.vue'
+import axios from 'axios'
 
 export default {
   components: {
@@ -21,6 +26,24 @@ export default {
     WorksSection,
     TestSection,
     AboutSection
+  },
+  data() {
+    return {
+      posts: 'default'
+    }
+  },
+  async asyncData() {
+    const { data } = await axios.get(
+      'https://shingo_hp.microcms.io/api/v1/blog',
+      {
+        headers: {
+          'X-API-KEY': '7103bff2-a42e-447e-a315-ed1020bcb61c'
+        }
+      }
+    )
+    return {
+      posts: data.contents[0]
+    }
   }
 }
 </script>
