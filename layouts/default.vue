18336efb2a63758a415917b7d9bbb344a2dc14ef
gentlable <35590706+gentlable@users.noreply.github.com>
2020-09-20 22:18:33 +0900

    JQuery追加、ビットマップフォント追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <script
+      src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+      integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
+      crossorigin="anonymous">
+    </script>
     <nuxt />
   </div>
 </template>
