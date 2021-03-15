6c92e4d0b566f2f4d5fa455df31d95cbbd78f975
gentlable <shingo.gt.1115@gmail.com>
2021-03-16 08:28:41 +0900

    プラグイン導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,8 @@ export default {
     microCmsApiKey,
     microCmsApiDomain
   },
-  mode: 'universal',
-//  mode: 'spa',
+  // mode: 'universal',
+  mode: 'spa',
   /*
    ** Headers of the page
    */
